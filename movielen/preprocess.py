4702592cd2b7f9a48dc250fb8c776f3ab6ac5392
Tri Nguyen <minhtribk@gmail.com>
2018-10-30 11:49:49 +0700

    test docker h6

--- movielen/preprocess.py
+++ movielen/preprocess.py
@@ -15,7 +15,7 @@ parsed_data = lines.map(lambda l: l.split(";"))
 record = parsed_data.map(lambda r: Row(timestamp=int(r[0]), content_id=int(r[1]), counter=int(r[2]), rating=float(r[3])))
 schema_record = spark.createDataFrame(record)
 
-df_rounded = transformation.round_timestamp(schema_record,24)
+df_rounded = transformation.round_timestamp(schema_record,48)
 df_grouped = transformation.group(df_rounded)
 df_derivative = transformation.derivative(df_grouped)
 df_derivative_2 = transformation.derivative_2(df_derivative)
@@ -29,5 +29,5 @@ df_sorted = transformation.sort_df(df_derivative_2)
 #df_sorted = transformation.sort_df(df_delta)
 #df_sorted = transformation.sort_df_by_count(df_filtered)
 
-df_sorted.repartition(1344, df_sorted["content_id"]).write.csv("data_partitioned_1day_derivative_2", sep=";", header=True)
+df_sorted.repartition(1344, df_sorted["content_id"]).write.csv("data_partitioned_2day_derivative_2", sep=";", header=True)
 
