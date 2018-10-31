680740e1a0e1a85d05c9eb5395939459e46df763
Tri Nguyen <minhtribk@gmail.com>
2018-10-31 20:16:51 +0700

    create request

---
+++ movielen/create_request.py
@@ -0,0 +1,48 @@
+import schema
+import transformation
+from pyspark.sql import SparkSession
+import os
+import tempfile
+from pyspark.sql import Row
+import argparse
+from pyspark.sql.functions import monotonically_increasing_id 
+
+spark = SparkSession.builder.getOrCreate()
+sc = spark.sparkContext
+
+filepath =("./data_filtered_5k/data_filtered.txt")
+
+# Parse data interval  (default interval: 5 days)
+parser = argparse.ArgumentParser()
+parser.add_argument("--interval", help="Insert interval to integrate data (days)")
+parser.add_argument("--timestamp", help="Insert timestamp to get requests")
+args = parser.parse_args()
+data_int = int(5) #default value
+data_time = int(1776) #default value
+if args.interval:
+    num = args.interval
+    if schema.toInt(num) is not None:
+        data_int = int(num)
+if args.timestamp:
+    timest = args.interval
+    if schema.toInt(timest) is not None:
+        data_time = int(timest)
+
+
+# Load a text file to RDD and convert each line to a Row.
+lines = sc.textFile(filepath)
+parsed_data = lines.map(lambda l: l.split(";"))
+record = parsed_data.map(lambda r: Row(timestamp=schema.toInt(r[0]), content_id=schema.toInt(r[1]), counter=schema.toInt(r[2]), rating=schema.toFloat(r[3])))
+
+# Create dataframe from RDD
+data_df = spark.createDataFrame(record, schema=schema.data_schema).na.drop()
+
+# Round time follow the interval
+df_rounded = transformation.round_timestamp(data_df,int(data_int))
+
+# Group data follow time interval
+df_filter = df_rounded.filter(df_rounded.timestamp == data_time)
+df_indexed = df_filter.withColumn("id", monotonically_increasing_id())
+df_cache_indexed = df_indexed.withColumn("id", df_indexed["id"] % 55)
+
+df_cache_indexed.repartition(1).write.csv("datacache_indexed_{}_days_interval".format(data_int), sep=";")
