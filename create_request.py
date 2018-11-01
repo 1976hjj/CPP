9d5d8ae6c6ca512499de30d52a656d53b05a2f9d
Tri Nguyen <minhtribk@gmail.com>
2018-11-01 16:56:02 +0700

    debug

--- create_request.py
+++ create_request.py
@@ -40,7 +40,7 @@ data_df = spark.createDataFrame(record, schema=schema.df_rounded_schema).na.drop
 
 
 # Group data follow time interval
-data_df = data_df.filter(data_df["timestamp"] == float(data_time))
+data_df = data_df.filter(data_df["timestamp"] == 1776)
 #df_indexed = df_filter.withColumn("id", monotonically_increasing_id())
 #df_cache_indexed = df_indexed.withColumn("id", df_indexed["id"] % 55)
 
