3787477f2b3ce6290c28737006be0e8bd8715b10
Tri Nguyen <minhtribk@gmail.com>
2018-11-01 17:32:35 +0700

    test final

--- generate_dataset.py
+++ generate_dataset.py
@@ -33,24 +33,24 @@ if args.interval:
     if toInt(num) is not None:
         data_int = int(num)
 
-dataset_path = glob.glob("./dataset_{}_days_interval/*.csv".format(data_int))
+dataset_path = glob.glob("./preprocess/dataset_{}_days_interval/*.csv".format(data_int))
 dataset_all = pd.read_csv(dataset_path[0],names=["timestamp","content_id","counts","d1", "d2", "label"], sep=";")
 
 df_grouped = dataset_all.groupby("content_id")
 
 for content_id, group in df_grouped:
     group = group.iloc[3:]
-    group[group.timestamp < (data_timestamp + 1)].to_csv("./data_separated/{}_train.csv".format(content_id), header=False, sep=";", index=False)
-    group[group.timestamp == (data_timestamp + 1)].to_csv("./data_separated/{}_test.csv".format(content_id), header=False, sep=";", index=False)
+    group[group.timestamp < (data_timestamp + 1)].to_csv("./result/data_separated/{}_train.csv".format(content_id), header=False, sep=";", index=False)
+    group[group.timestamp == (data_timestamp + 1)].to_csv("./result/data_separated/{}_test.csv".format(content_id), header=False, sep=";", index=False)
 
-datalist_path = glob.glob("./datalist_{}_days_interval/*.csv".format(data_int))
+datalist_path = glob.glob("./preprocess/datalist_{}_days_interval/*.csv".format(data_int))
 datalist_all = pd.read_csv(datalist_path[0],names=["timestamp","content_id","counts","const"], sep=";")
 
 data_filtered = datalist_all[datalist_all.timestamp == data_timestamp]
 data_sorted = data_filtered[["counts", "content_id"]].sort_values(by=["counts"],ascending=False)
-data_sorted.to_csv("./content_popularity/timestamp_{}.csv".format(data_timestamp), header=False, sep=";", index=False)
+data_sorted.to_csv("./result/content_popularity/timestamp_{}.csv".format(data_timestamp), header=False, sep=";", index=False)
 
-content_popularity_path = glob.glob("./content_popularity_all/*.csv".format(data_int))
+content_popularity_path = glob.glob("./preprocess/content_popularity_all/*.csv".format(data_int))
 list_content = pd.read_csv(content_popularity_path[0],names=["counts","content_id"], sep=";")
 list_content_id = list_content[["content_id"]]
 
@@ -58,18 +58,18 @@ timestamp_ = data_timestamp+1
 
 def train_by_content_id(row):
     if row is not None:
-        data_train = pd.read_csv("./data_separated/{}_train.csv".format(row["content_id"]),names=["timestamp","content_id","counts","d1", "d2", "label"], sep=";")
+        data_train = pd.read_csv("./result/data_separated/{}_train.csv".format(row["content_id"]),names=["timestamp","content_id","counts","d1", "d2", "label"], sep=";")
 
         X_train = data_train[["counts", "d1", "d2"]]
         y_train = data_train["label"]
-        if X_train.shape[0] > 2:
+        if X_train.shape[0] < 2:
             return 0
 
-        data_test = pd.read_csv("./data_separated/{}_test.csv".format(row["content_id"]),names=["timestamp","content_id","counts","d1", "d2", "label"], sep=";")
+        data_test = pd.read_csv("./result/data_separated/{}_test.csv".format(row["content_id"]),names=["timestamp","content_id","counts","d1", "d2", "label"], sep=";")
 
         X_test = data_test[["counts", "d1", "d2"]]
         y_test = data_test["label"]
-        if X_test.shape[0] > 1:
+        if X_test.shape[0] < 1:
             return 0
         regressor = LinearRegression()  
         regressor.fit(X_train, y_train) 
@@ -81,12 +81,12 @@ list_content_id["counts"] = list_content_id.apply(lambda row: train_by_content_i
 list_content_id.counts = list_content_id.counts.round()
 data_sorted = list_content_id.sort_values(by=["counts"],ascending=False)
 data_sorted = data_sorted[["counts","content_id"]]
-data_sorted.to_csv("./content_popularity/timestamp_{}.csv".format(timestamp_), header=False, sep=";", index=False)
+data_sorted.to_csv("./result/content_popularity/timestamp_{}.csv".format(timestamp_), header=False, sep=";", index=False)
 
 
-dataset_all_path = glob.glob("datacache_indexed_{}_days_interval/*.csv".format(data_int))
+dataset_all_path = glob.glob("./preprocess/datacache_indexed_{}_days_interval/*.csv".format(data_int))
 dataset_all = pd.read_csv(dataset_all_path[0],names=["timestamp", "content_id", "timestamp_", "cache"], sep=";")
 
 df_grouped = dataset_all.groupby("cache")
 for cache, group in df_grouped:
-    group[["content_id"]].to_csv("./cache/Cache_{}.csv".format(cache+1), header=False, sep=";", index=False)
+    group[["content_id"]].to_csv("./result/cache/Cache_{}.csv".format(cache+1), header=False, sep=";", index=False)
